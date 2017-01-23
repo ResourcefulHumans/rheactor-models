677c0b9bdfec9968c51f0b52240bb50970bb29e2
Markus Tacker <m@coderbyheart.com>
2017-01-23 15:58:18 +0100

    feat(Reference): added

---
+++ src/reference.js
@@ -0,0 +1,63 @@
+import {String as StringType, irreducible, struct, refinement} from 'tcomb'
+import {URIValue, URIValueType} from 'rheactor-value-objects'
+const $context = new URIValue('https://github.com/ResourcefulHumans/rheactor-models#Reference')
+
+export class Reference {
+  /**
+   * @param {URIValue} $id The id of the referenced item, which is also the URL to retrieve the reference
+   * @param {URIValue} subject The context of the referenced item
+   */
+  constructor ($id, subject) {
+    URIValueType($id)
+    URIValueType(subject)
+    this.$context = $context
+    this.$id = $id
+    this.subject = subject
+  }
+
+  /**
+   * @returns {{$context: String, subject: String, $id: String}}}
+   */
+  toJSON () {
+    const d = {
+      $context: this.$context.toString(),
+      subject: this.subject.toString(),
+      $id: this.$id.toString()
+    }
+    return d
+  }
+
+  /**
+   * @param {{$context: String, subject: String, $id: String}} data
+   * @returns {Reference}
+   */
+  static fromJSON (data) {
+    ReferenceJSONType(data)
+    const {$id, subject} = data
+    return new Reference(new URIValue($id), new URIValue(subject))
+  }
+
+  /**
+   * @returns {URIValue}
+   */
+  static get $context () {
+    return $context
+  }
+
+  /**
+   * Returns true if x is of type Reference
+   *
+   * @param {object} x
+   * @returns {boolean}
+   */
+  static is (x) {
+    return (x instanceof Reference) || (x && x.constructor && x.constructor.name === Reference.name && '$context' in x && URIValue.is(x.$context) && $context.equals(x.$context))
+  }
+}
+
+export const ReferenceJSONType = struct({
+  $context: refinement(StringType, s => s === $context.toString(), 'ReferenceContext'),
+  subject: StringType,
+  $id: StringType
+}, 'ReferenceJSONType')
+export const ReferenceType = irreducible('ReferenceType', Reference.is)
---
+++ test/reference.spec.js
@@ -0,0 +1,46 @@
+'use strict'
+
+/* global describe, it */
+
+import {expect} from 'chai'
+import {Reference, ReferenceType} from '../src'
+import {URIValue} from 'rheactor-value-objects'
+
+function validateReference (reference) {
+  ReferenceType(reference)
+  expect(reference.$id.equals(new URIValue('http://example.com/some-item/42'))).to.equal(true)
+  expect(reference.subject.equals(new URIValue('http://example.com/jsonld/some'))).to.equal(true)
+  expect(reference.$context.equals(Reference.$context)).to.equal(true)
+}
+describe('Reference', () => {
+  describe('constructor()', () => {
+    it('should accept values', () => {
+      const reference = new Reference(new URIValue('http://example.com/some-item/42'), new URIValue('http://example.com/jsonld/some'))
+      validateReference(reference)
+    })
+    it('should parse it\'s own values', () => {
+      const reference = new Reference(
+        new URIValue('http://example.com/some-item/42'),
+        new URIValue('http://example.com/jsonld/some')
+      )
+      const reference2 = new Reference(
+        reference.$id,
+        reference.subject
+      )
+      validateReference(reference2)
+    })
+  })
+
+  describe('JSON', () => {
+    it('should parse it\'s JSON representation', () => {
+      const reference = Reference.fromJSON(JSON.parse(JSON.stringify(new Reference(new URIValue('http://example.com/some-item/42'), new URIValue('http://example.com/jsonld/some')))))
+      validateReference(reference)
+    })
+  })
+
+  describe('$context', () => {
+    it('should exist', () => {
+      expect(Reference.$context.toString()).to.equal('https://github.com/ResourcefulHumans/rheactor-models#Reference')
+    })
+  })
+})
